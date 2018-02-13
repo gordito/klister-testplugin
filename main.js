18281265ff1388ab1e16583d21fedc98b9b4b504
Robin Kochauf <robin@gordito.se>
2018-02-13 14:39:17 +0100

    Command tests

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
+/*
+	A testcommand, exports Init and Run
+*/
 
+var Controller = null;
+var Session = null;
 
-
-
-function Test()
+function Init(c, s)
 {
-
+	Controller = c;
+	Session = s;
 	console.log('test plugin');
-
+	console.log(Controller);
+	console.log(Session);
 }
 
-module.exports = Test;
+function Run(com, req, resp)
+{
+	console.log('== TESTCOMMAND RUN ==');
+	console.log(com);
+	console.log(req);
+	console.log(resp);
+}
+
+module.exports = {
+	Init,
+	Run
+};
